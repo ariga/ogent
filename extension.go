318356b51a89743ad2cfd0440eaad7b29a4693dc
Jannik C <github@masseelch.de>
2021-12-17 21:02:06 +0100

    use ogen spec definitions directly

--- extension.go
+++ extension.go
@@ -2,31 +2,108 @@ package ogent
 
 import (
 	"errors"
-	"io"
+	"fmt"
+	"go/format"
+	"os"
+	"path/filepath"
 
 	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+	"github.com/ogen-go/ogen"
+	ogengen "github.com/ogen-go/ogen/gen"
 )
 
 type (
 	// Extension implements entc.Extension interface providing integration with ogen.
 	Extension struct {
 		entc.DefaultExtension
-		src io.Reader
+		// The OpenAPI Specification to generate handler implementations for.
+		spec *ogen.Spec
+		// target holds the filepath to write the ogen assets to.
+		target string
+		// pkg holds the package name of the ogen assets.
+		pkg string
 	}
 	// ExtensionOption allows managing Extension configuration using functional arguments
 	ExtensionOption func(*Extension) error
 )
 
 // NewExtension returns a new ogent extension with default configuration.
-func NewExtension(src io.Reader, opts ...ExtensionOption) (*Extension, error) {
-	if src == nil {
-		return nil, errors.New("ogent: src cannot be nil")
+func NewExtension(spec *ogen.Spec, opts ...ExtensionOption) (*Extension, error) {
+	if spec == nil {
+		return nil, errors.New("ogent: spec cannot be nil")
 	}
-	ex := &Extension{src: src}
+	ex := &Extension{spec: spec}
 	for _, opt := range opts {
 		if err := opt(ex); err != nil {
 			return nil, err
 		}
 	}
 	return ex, nil
+}
+
+// Target sets the directory the ogen assets are written to.
+func Target(t string) ExtensionOption {
+	return func(ex *Extension) error {
+		ex.target = t
+		return nil
+	}
+}
+
+// Package sets the package name for the ogen assets.
+func Package(pkg string) ExtensionOption {
+	return func(ex *Extension) error {
+		ex.pkg = pkg
+		return nil
+	}
+}
+
+func (ex Extension) Hooks() []gen.Hook {
+	return []gen.Hook{
+		ex.ogen,
+	}
+}
+
+func (ex Extension) ogen(next gen.Generator) gen.Generator {
+	return gen.GenerateFunc(func(g *gen.Graph) error {
+		// Let ent create all of its assets.
+		if err := next.Generate(g); err != nil {
+			return err
+		}
+		// Ensure target exists.
+		t := ex.target
+		if t == "" {
+			t = filepath.Join(g.Target, "api")
+		}
+		_, err := os.Stat(t)
+		if err != nil && !os.IsNotExist(err) {
+			return fmt.Errorf("ogent: read target dir: %w", err)
+		}
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(t, 0750); err != nil {
+				return fmt.Errorf("ogent: create target dir: %w", err)
+			}
+		}
+		// Ensure there is a package name given.
+		pkg := ex.pkg
+		if pkg == "" {
+			pkg = "api"
+		}
+		// Run the ogen code generator.
+		generator, err := ogengen.NewGenerator(ex.spec, ogengen.Options{})
+		if err != nil {
+			return err
+		}
+		return generator.WriteSource(formatFS{t}, pkg)
+	})
+}
+
+type formatFS struct{ Root string }
+
+func (f formatFS) WriteFile(name string, content []byte) error {
+	buf, err := format.Source(content)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filepath.Join(f.Root, name), buf, 0600)
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,33 @@ module github.com/ariga/ogent
 go 1.17
 
 require (
-	entgo.io/contrib v0.2.0
-	entgo.io/ent v0.9.2-0.20210821141344-368a8f7a2e9a
+	entgo.io/ent v0.9.2-0.20211216115003-1c263c7abd10
+	github.com/go-faster/errors v0.5.0
+	github.com/go-faster/jx v0.25.0
+	github.com/google/uuid v1.3.0
+	github.com/ogen-go/ogen v0.0.0-00010101000000-000000000000
+	go.opentelemetry.io/otel v1.3.0
+	go.opentelemetry.io/otel/metric v0.26.0
+	go.opentelemetry.io/otel/trace v1.3.0
 )
 
+require golang.org/x/sys v0.0.0-20211113001501-0c823b97ae02 // indirect
+
 require (
+	entgo.io/contrib v0.0.0-00010101000000-000000000000 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-logr/logr v1.2.1 // indirect
+	github.com/go-logr/stdr v1.2.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/stoewer/go-strcase v1.2.0 // indirect
+	github.com/goccy/go-yaml v1.9.4 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/mod v0.5.1 // indirect
-	golang.org/x/sys v0.0.0-20211113001501-0c823b97ae02 // indirect
-	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/tools v0.1.8 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
+
+replace github.com/ogen-go/ogen => github.com/masseelch/ogen v0.0.0-20211217142721-bb02574f0aab
+
+replace entgo.io/contrib => entgo.io/contrib v0.2.1-0.20211217200019-533305064701
