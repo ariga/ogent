c1955a2f931a3d984de6cad1d58ab53cbe3abf5a
MasseElch <12862103+masseelch@users.noreply.github.com>
2021-12-18 08:39:02 +0100

    use ogen spec definitions directly (#6)
    * use ogen spec definitions directly
    
    * make config accessible in code generation
    
    * make package name unconfigurable

--- example/pets/entc.go
+++ example/pets/entc.go
@@ -4,26 +4,26 @@
 package main
 
 import (
-	"io"
 	"log"
 
 	"entgo.io/contrib/entoas"
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 	"github.com/ariga/ogent"
+	"github.com/ogen-go/ogen"
 )
 
 func main() {
-	r, w := io.Pipe()
-	oas, err := entoas.NewExtension(entoas.WriteTo(w))
+	spec := new(ogen.Spec)
+	oas, err := entoas.NewExtension(entoas.Spec(spec))
 	if err != nil {
 		log.Fatalf("creating entoas extension: %v", err)
 	}
-	ogen, err := ogent.NewExtension(r)
+	ogen, err := ogent.NewExtension(spec)
 	if err != nil {
 		log.Fatalf("creating ogent extension: %v", err)
 	}
-	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(oas))
+	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ogen, oas))
 	if err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
--- extension.go
+++ extension.go
@@ -2,27 +2,41 @@ package ogent
 
 import (
 	"errors"
-	"io"
+	"fmt"
+	"go/format"
+	"os"
+	"path/filepath"
 
 	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+	"github.com/ogen-go/ogen"
+	ogengen "github.com/ogen-go/ogen/gen"
 )
 
 type (
+	// Config is injected into the code generation templates.
+	Config struct {
+		// Target holds the filepath to write the ogen assets to.
+		Target string
+	}
 	// Extension implements entc.Extension interface providing integration with ogen.
 	Extension struct {
 		entc.DefaultExtension
-		src io.Reader
+		// The OpenAPI Specification to generate handler implementations for.
+		spec *ogen.Spec
+		// Code generation configuration.
+		cfg *Config
 	}
 	// ExtensionOption allows managing Extension configuration using functional arguments
 	ExtensionOption func(*Extension) error
 )
 
 // NewExtension returns a new ogent extension with default configuration.
-func NewExtension(src io.Reader, opts ...ExtensionOption) (*Extension, error) {
-	if src == nil {
-		return nil, errors.New("ogent: src cannot be nil")
+func NewExtension(spec *ogen.Spec, opts ...ExtensionOption) (*Extension, error) {
+	if spec == nil {
+		return nil, errors.New("ogent: spec cannot be nil")
 	}
-	ex := &Extension{src: src}
+	ex := &Extension{spec: spec, cfg: new(Config)}
 	for _, opt := range opts {
 		if err := opt(ex); err != nil {
 			return nil, err
@@ -30,3 +44,62 @@ func NewExtension(src io.Reader, opts ...ExtensionOption) (*Extension, error) {
 	}
 	return ex, nil
 }
+
+// Target sets the directory the ogen assets are written to.
+func Target(t string) ExtensionOption {
+	return func(ex *Extension) error {
+		ex.cfg.Target = t
+		return nil
+	}
+}
+
+// Hooks of the extension.
+func (ex Extension) Hooks() []gen.Hook {
+	return []gen.Hook{
+		ex.ogen,
+	}
+}
+
+func (ex Extension) ogen(next gen.Generator) gen.Generator {
+	return gen.GenerateFunc(func(g *gen.Graph) error {
+		// Let ent create all of its assets.
+		if err := next.Generate(g); err != nil {
+			return err
+		}
+		// Ensure target exists.
+		t := ex.cfg.Target
+		if t == "" {
+			t = filepath.Join(g.Target, "api")
+		}
+		_, err := os.Stat(t)
+		if err != nil && !os.IsNotExist(err) {
+			return fmt.Errorf("ogent: read target dir: %w", err)
+		}
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(t, 0750); err != nil {
+				return fmt.Errorf("ogent: create target dir: %w", err)
+			}
+		}
+		// Run the ogen code generator.
+		generator, err := ogengen.NewGenerator(ex.spec, ogengen.Options{})
+		if err != nil {
+			return err
+		}
+		return generator.WriteSource(formatFS{t}, "api")
+	})
+}
+
+// Name implements the entc.Annotation interface.
+func (Config) Name() string { return "OgentCfg" }
+
+type formatFS struct{ Root string }
+
+func (f formatFS) WriteFile(name string, content []byte) error {
+	buf, err := format.Source(content)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filepath.Join(f.Root, name), buf, 0600)
+}
+
+var _ entc.Annotation = (*Config)(nil)
