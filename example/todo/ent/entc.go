c4d586fc1c16126a5d5f7c8f574d8e153a96de95
MasseElch <12862103+masseelch@users.noreply.github.com>
2022-02-17 09:18:16 +0100

    example: add todo app from blog post (#40)

---
+++ example/todo/ent/entc.go
@@ -0,0 +1,49 @@
+//go:build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/contrib/entoas"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+	"github.com/ariga/ogent"
+	"github.com/ogen-go/ogen"
+)
+
+func main() {
+	spec := new(ogen.Spec)
+	oas, err := entoas.NewExtension(
+		entoas.Spec(spec),
+		entoas.Mutations(func(_ *gen.Graph, spec *ogen.Spec) error {
+			spec.AddPathItem("/todos/{id}/done", ogen.NewPathItem().
+				SetDescription("Mark an item as done").
+				SetPatch(ogen.NewOperation().
+					SetOperationID("markDone").
+					SetSummary("Marks a todo item as done.").
+					AddTags("Todo").
+					AddResponse("204", ogen.NewResponse().SetDescription("Item marked as done")),
+				).
+				AddParameters(ogen.NewParameter().
+					InPath().
+					SetName("id").
+					SetRequired(true).
+					SetSchema(ogen.Int()),
+				),
+			)
+			return nil
+		}),
+	)
+	if err != nil {
+		log.Fatalf("creating entoas extension: %v", err)
+	}
+	ogent, err := ogent.NewExtension(spec)
+	if err != nil {
+		log.Fatalf("creating ogent extension: %v", err)
+	}
+	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ogent, oas))
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
---
+++ example/todo/ent/schema/todo.go
@@ -0,0 +1,20 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("title"),
+		field.Bool("done").
+			Optional(),
+	}
+}
